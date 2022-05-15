f8cda757f2b26c66b22f62c29dd4d074dc0e7a14
Florian <92161528+peflor@users.noreply.github.com>
2022-05-15 22:08:04 +0200

    tableau en dur Games

--- games.php
+++ games.php
@@ -7,3 +7,37 @@
     $title="Liste de jeux";
     require_once "base.html.php"
 ?>
+
+<div class="container">
+    <table class="table table-hover text-center">
+        <thead class="table-dark">
+            <tr>
+                <th>Titre</th>
+                <th>Nombre de joueurs</th>
+                <th colspan="2">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Starcraft 2</td>
+                <td>8</td>
+                <td><a href=""><i class="fas fa-edit"></i></a></td>
+                <td><a href=""><i class="fas fa-trash"></i></a></td>
+            </tr>
+            <tr>
+                <td>Among Us</td>
+                <td>10</td>
+                <td><a href=""><i class="fas fa-edit"></i></a></td>
+                <td><a href=""><i class="fas fa-trash"></i></a></td>
+            </tr>
+            <tr>
+                <td>Valorant</td>
+                <td>10</td>
+                <td><a href=""><i class="fas fa-edit"></i></a></td>
+                <td><a href=""><i class="fas fa-trash"></i></a></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<a href="" class="btn btn-success w-25 d-block m-auto">Ajouter un jeu</a>
