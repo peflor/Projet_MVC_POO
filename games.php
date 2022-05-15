69734b1838bcb3175ae3c40ba3bb174247436df7
Florian <92161528+peflor@users.noreply.github.com>
2022-05-15 23:40:37 +0200

    Update games.php

--- games.php
+++ games.php
@@ -8,6 +8,12 @@ ob_start() ?>
 
 <p>Games - Notre sélection</p>
 
+<?php 
+    $content= ob_get_clean();
+    $title="Liste de jeux";
+    require_once "base.html.php";
+
+?>
 
 
 <div class="container">
