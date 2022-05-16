f51ec39bd438cab49084b87c82b057f85ad32f0c
Florian <92161528+peflor@users.noreply.github.com>
2022-05-16 16:00:43 +0200

    URL dynamique

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
+define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS']) ? "https" : "http")."://".$_SERVER['HTTP_HOST']. $_SERVER['PHP_SELF']));
+
 
 require_once "controller/GameController.php";
 $gameController = new GameController;
@@ -6,6 +8,7 @@ $gameController = new GameController;
 if(empty($_GET['page'])){
     require_once "./view/home.view.php";
 }else {
+    $url = explode("/", filter_var($_GET['page']), FILTER_SANITIZE_URL);    
     switch($_GET['page']){
         case "accueil" : require_once "./view/home.view.php";
         break;
