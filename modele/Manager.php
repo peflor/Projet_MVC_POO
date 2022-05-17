3f5559e866d515256e1f370de0b3c75f2123a8d8
Florian <92161528+peflor@users.noreply.github.com>
2022-05-17 09:08:22 +0200

    Update mdp bdd localhost win

--- modele/Manager.php
+++ modele/Manager.php
@@ -5,7 +5,7 @@ abstract class Manager {
     private $bdd;
 
     private function setBdd(){
-        $this->bdd = new PDO("mysql:host=localhost;dbname=game_x;charset=utf8","root","root");
+        $this->bdd = new PDO("mysql:host=localhost;dbname=game_x;charset=utf8","root","");
         $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
